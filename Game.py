be153fa5ae1bbe01c46368d6e6b162d42b6ce1f3
Zachary <zdean1995@gmail.com>
2024-04-09 22:05:48 -0400

    Added exp drops and leveling up. Leveling up currently doesn't do anything.

--- Game.py
+++ Game.py
@@ -1,7 +1,9 @@
 import pygame
-import random
+from random import randint, choice
 import math
 from os.path import join
+
+from pygame import Vector2
 
 # Initialize Pygame
 pygame.init()
@@ -16,6 +18,8 @@ pygame.display.set_caption("Auto Shooter")
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 BLACK = (0, 0, 0)
+
+damage_font = pygame.font.Font('assets/font/Pixeltype.ttf', 50)
 
 
 class Player(pygame.sprite.Sprite):
@@ -39,6 +43,7 @@ class Player(pygame.sprite.Sprite):
             self.y += 10
 
     def update(self):
+        global exp_drops
         self.player_input()
 
 
@@ -50,7 +55,7 @@ class Bullet(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(center=(WIDTH // 2, HEIGHT // 2))
         self.y_vel = 0
         self.x_vel = 0
-        self.range = 10
+        self.range = 25
         self.lifetime = 0
         match direction:
             case "up":
@@ -78,21 +83,83 @@ class Enemy(pygame.sprite.Sprite):
         global offset_y, offset_x
         self.image = pygame.Surface((50, 50))
         self.image.fill(BLACK)
+        self.hurt_frames = 0
         self.rect = self.image.get_rect(center=
-                                        ((random.randint(0, WIDTH) + offset_x)
-                                         , (random.randint(0, HEIGHT) + offset_y)))
+                                        ((randint(0, WIDTH) + offset_x)
+                                         , (randint(0, HEIGHT) + offset_y)))
+        self.health = 3
+
+    def drop_exp(self):
+        global exp_drops
+        exp_drops.add(ExpDrop(10, self.rect.x, self.rect.y))
 
     def update(self):
-        global bullets
+        global bullets, damage_numbers, enemies
         dx, dy = CENTER_WIDTH - self.rect.x, CENTER_HEIGHT - self.rect.y
         dist = math.hypot(dx, dy)
         dx, dy = dx / dist, dy / dist
         self.rect.x += dx - x_vel
         self.rect.y += dy - y_vel
-        if pygame.sprite.spritecollide(self, bullets, False):
+        if self.hurt_frames > 0:
+            self.hurt_frames -= 1
+        elif pygame.sprite.spritecollide(self, bullets, False):
+            self.hurt_frames = 5
+            if self.health == 1:
+                self.drop_exp()
+                self.drop_exp()
+                self.drop_exp()
+                self.kill()
+            else:
+                self.health -= 1
+            damage_numbers.add(DamageNumber("1", self.rect.x, self.rect.y))
+
+class DamageNumber(pygame.sprite.Sprite):
+    def __init__(self, num, x, y):
+        super().__init__()
+        self.image = damage_font.render(num, False, RED)
+        self.rect = self.image.get_rect(center = (x, y))
+        self.lifetime = 0
+
+    def update(self):
+        self.rect.y -= y_vel + 3
+        if self.lifetime >= 40:
             self.kill()
+        elif 10 >= self.lifetime > 5 or 20 >= self.lifetime > 15 or 30 >= self.lifetime > 25 or self.lifetime > 35:
+            self.rect.x -= x_vel + 3
+        else:
+            self.rect.x -= x_vel - 3
+        self.lifetime += 1
+        self.image.set_alpha(180 - (self.lifetime * 6))
 
 
+class ExpDrop(pygame.sprite.Sprite):
+    def __init__(self, num, x, y):
+        super().__init__()
+        self.image = pygame.Surface((10, 10))
+        self.image.fill(WHITE)
+        self.exp_amount = num
+        self.rect = self.image.get_rect(center=(x, y))
+        self.move_time = 10
+        self.x_vel = randint(0, 10)
+        self.y_vel = randint(0, 10)
+
+    def update(self):
+        global player
+        if Vector2(player.sprite.rect.center).distance_to(self.rect.center) < 100:
+            dx, dy = CENTER_WIDTH - self.rect.x, CENTER_HEIGHT - self.rect.y
+            dist = math.hypot(dx, dy)
+            dx, dy = dx / dist, dy / dist
+            self.rect.x += dx * 10 - x_vel
+            self.rect.y += dy * 10 - y_vel
+        elif self.move_time > 0:
+            self.rect.x += self.x_vel
+            self.rect.y += self.y_vel
+            self.move_time -= 1
+        self.rect.x -= x_vel
+        self.rect.y -= y_vel
+        if pygame.sprite.spritecollide(self, player, False):
+            exp_check(self.exp_amount)
+            self.kill()
 
 def get_background(name):
     image = pygame.image.load(join("assets", "Background", name))
@@ -107,11 +174,39 @@ def get_background(name):
     return tiles, image
 
 
+def exp_check(amount):
+    global exp, level_up_exp, exp_bar_progress
+    exp += amount
+    if exp >= level_up_exp:
+        exp -= level_up_exp
+        level_up_exp *= 1.5
+        exp_bar_progress = pygame.Surface((0, 20))
+        exp_check(0)
+    else:
+        exp_bar_progress = pygame.Surface(((exp/level_up_exp)*(WIDTH * 0.9), 20))
+        exp_bar_progress.fill(WHITE)
+
+
+
+
 player = pygame.sprite.GroupSingle()
 player.add(Player())
 
 bullets = pygame.sprite.Group()
 enemies = pygame.sprite.Group()
+damage_numbers = pygame.sprite.Group()
+exp_drops = pygame.sprite.Group()
+
+#exp values
+exp = 0
+level_up_exp = 1000
+
+exp_bar = pygame.Surface((WIDTH * 0.9, 20))
+
+exp_bar_progress = pygame.Surface((0, 20))
+exp_bar_progress.fill(WHITE)
+
+
 
 # Game loop
 clock = pygame.time.Clock()
@@ -122,7 +217,7 @@ bullet_timer = pygame.USEREVENT + 1
 pygame.time.set_timer(bullet_timer, 250)
 
 enemy_timer = pygame.USEREVENT + 2
-pygame.time.set_timer(enemy_timer, 1000)
+pygame.time.set_timer(enemy_timer, 500)
 
 y_vel = 0
 x_vel = 0
@@ -142,6 +237,13 @@ while running:
     bullets.update()
     enemies.draw(screen)
     enemies.update()
+    damage_numbers.draw(screen)
+    damage_numbers.update()
+    exp_drops.draw(screen)
+    exp_drops.update()
+
+    screen.blit(exp_bar, (WIDTH*0.05, HEIGHT*0.9))
+    screen.blit(exp_bar_progress, (WIDTH*0.05, HEIGHT*0.9))
 
     # Event handling
     for event in pygame.event.get():
